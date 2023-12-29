18a2da2f9dba634f9dba8a271f1cb770c9768436
Rafael Lebre <rafaellebre100@gmail.com>
2023-12-29 20:14:21 +0100

    Aula 80: Processando letras certas e erradas

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import { useCallback, useEffect, useState } from 'react';
 
 // data
-import {wordsList} from "./data/words";
+import { wordsList } from "./data/words";
 
 // components
 import StartScreen from './components/StartScreen';
@@ -13,9 +13,9 @@ import Game from './components/Game';
 import GameOver from './components/GameOver';
 
 const stages = [
-  {id: 1, name: "start"},
-  {id: 2, name: "game"},
-  {id: 3, name: "end"},
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
 ];
 
 function App() {
@@ -41,54 +41,77 @@ function App() {
     // pick a random word
     const word = words[category][Math.floor(Math.random() * words[category].length)];
 
-console.log(word);
+    console.log(word);
 
-return {word, category};
+    return { word, category };
   }
 
   // starts the secret word game
- const startGame = () => {
-  // pick word and pick category
-  const {word, category} = pickWordAndCategory();
+  const startGame = () => {
+    // pick word and pick category
+    const { word, category } = pickWordAndCategory();
 
-  // create an array of letters
-  let wordLetters = word.split("")
+    // create an array of letters
+    let wordLetters = word.split("")
 
-  wordLetters = wordLetters.map((l) => l.toLowerCase());
-  
-  console.log(word, category);
-  console.log(wordLetters);
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
 
-  // fill states
-  setPickedWord(word);
-  setPickedCategory(category);
-  setLetters(wordLetters);
+    console.log(word, category);
+    console.log(wordLetters);
 
-  setGameStage(stages[1].name);
- };
+    // fill states
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
 
- // process the letter input
- const verifyLetter = (letter) => {
-  console.log(letter);
- };
+    setGameStage(stages[1].name);
+  };
 
- // restarts the game
- const retry = () => {
-  setGameStage(stages[0].name);
- };
+  // process the letter input
+  const verifyLetter = (letter) => {
+
+    const normalizedLetter = letter.toLowerCase()
+
+    // check if letter has already been utilized
+    if (
+      guessedLetters.includes(normalizedLetter) ||
+      wrongLetters.includes(normalizedLetter)) { return; }
+
+    // push guessed letter or remove a guess
+    if (letters.includes(normalizedLetter)) {
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        normalizedLetter
+      ])
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter
+      ]);
+    }
+
+  };
+
+  console.log(guessedLetters);
+  console.log(wrongLetters);
+
+  // restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
 
   return (
     <div className="App">
       {gameStage === "start" && <StartScreen startGame={startGame} />}
-      {gameStage === "game" && <Game 
-      verifyLetter={verifyLetter} 
-      pickedWord={pickedWord} 
-      pickedCategory={pickedCategory} 
-      letters={letters} 
-      guessedLetters={guessedLetters}
-      wrongLetters={wrongLetters}
-      guesses={guesses}
-      score={score}
+      {gameStage === "game" && <Game
+        verifyLetter={verifyLetter}
+        pickedWord={pickedWord}
+        pickedCategory={pickedCategory}
+        letters={letters}
+        guessedLetters={guessedLetters}
+        wrongLetters={wrongLetters}
+        guesses={guesses}
+        score={score}
       />}
       {gameStage === "end" && <GameOver retry={retry} />}
     </div>
