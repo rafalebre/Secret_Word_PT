21079f887e754d5f47bd4ad8c2037766a84d6c3b
Rafael Lebre <rafaellebre100@gmail.com>
2023-12-29 21:35:56 +0100

    Aula 82: Finalizando tela de fim de jogo

--- src/App.js
+++ src/App.js
@@ -133,7 +133,7 @@ function App() {
         guesses={guesses}
         score={score}
       />}
-      {gameStage === "end" && <GameOver retry={retry} />}
+      {gameStage === "end" && <GameOver retry={retry} score={score}/>}
     </div>
   );
 }
